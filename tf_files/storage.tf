86ae28aca304bedc0770e8670bc5ca5adeba98d7
albertdwtg <dewatrigant.albert@gmail.com>
2023-12-26 12:00:04 +0100

    change bucket test name

--- tf_files/storage.tf
+++ tf_files/storage.tf
@@ -1,7 +1,7 @@
 
 resource "google_storage_bucket" "raw" {
   project = var.project_id
-  name = "raw"
+  name = "raw_test_crypto_strategy"
   force_destroy = false
   uniform_bucket_level_access = true
   location = var.region
